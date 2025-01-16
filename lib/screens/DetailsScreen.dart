ead85c710c9eaa7d075ec03c2112ce3c92c400d2
abdo taouti <155262563+abdoott@users.noreply.github.com>
2025-01-16 03:00:23 +0100

    ajouter l'interface DetailsScreen.dart

--- lib/screens/DetailsScreen.dart
+++ lib/screens/DetailsScreen.dart
@@ -1,18 +1,49 @@
-
 import 'package:flutter/material.dart';
 
-class DetailsScreen extends StatefulWidget {
-  const DetailsScreen({super.key});
+class DetailsScreen extends StatelessWidget {
+  final String title;
+  final String description;
+  final String category;
+  final double amount;
+  final DateTime date;
 
-  @override
-  State<DetailsScreen> createState() => _DetailsScreenState();
-}
+  const DetailsScreen({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.amount,
+    required this.date,
+  });
 
-class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Text('Details'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Title: $title',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
+            Text('Description: $description',
+                style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 8),
+            Text('Category: $category', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 8),
+            Text('Amount: $amount \$', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 8),
+            Text('Date: ${date.toLocal().toString().split(' ')[0]}',
+                style: const TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
     );
   }
 }
