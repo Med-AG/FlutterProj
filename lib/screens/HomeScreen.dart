fad66a36d27cd40d1e5eae679c366a326110a423
abdo taouti <155262563+abdoott@users.noreply.github.com>
2025-01-16 03:01:12 +0100

    ajouter l'interface HomeScreen.dart

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,9 +1,7 @@
-import 'package:expense_income_tracker/models/StatsDTO.dart';
-import 'package:expense_income_tracker/models/GraphDTO.dart';
-import 'package:expense_income_tracker/service/api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
+import 'package:statefulclickcounter/screens/AddExpenseScreen.dart';
+import 'package:statefulclickcounter/screens/AddIncomeScreen.dart';
+import 'package:statefulclickcounter/screens/DetailsScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,36 +11,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  StatsDTO? stats; // Store stats data
-  GraphDTO? graphData; // Store chart data
-  bool isLoading = true;
-  String errorMessage = '';
+  int _selectedIndex = 0; // Track the selected navigation bar index
 
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
+  // List of widgets for different screens
+  final List<Widget> _pages = [
+    const HomePage(),
+    const AddIncomeScreen(),
+    const AddExpenseScreen(),
+  ];
 
-  Future<void> fetchData() async {
-    try {
-      // Call both API functions concurrently using Future.wait()
-      final results = await Future.wait([
-        ApiService().fetchStats(), // Fetch stats data
-        ApiService().fetchChartData(), // Fetch chart data
-      ]);
-
-      setState(() {
-        stats = results[0] as StatsDTO;
-        graphData = results[1] as GraphDTO;
-        isLoading = false; // Data has been fetched, update the state
-      });
-    } catch (error) {
-      setState(() {
-        errorMessage = 'Failed to fetch data. Please try again.';
-        isLoading = false;
-      });
-    }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex =
+          index; // Change the selected index to display the corresponding page
+    });
   }
 
   @override
@@ -60,170 +42,140 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.deepPurple,
       ),
       body: SafeArea(
-        child: isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : errorMessage.isNotEmpty
-            ? Center(child: Text(errorMessage))
-            : Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              // First Container with stats info
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple.shade100,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(0, 3),
-                      blurRadius: 5,
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    _buildInfoCard("Incomes", "${stats?.totalIncome} \$", Colors.green),
-                    _buildInfoCard("Balance", "${stats?.balance} \$", Colors.blue),
-                    _buildInfoCard("Expenses", "${stats?.totalExpense} \$", Colors.red),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 15),
-              // Second part: List of transactions (or a static example here)
-              Expanded(
-                child: ListView(
-                  children: [
-                    // Display expenses
-                    Column(
-                      children: graphData?.expenseList.map((expense) {
-                        // Format the DateTime to string with desired format
-                        String formattedDate = DateFormat('yyyy-MM-dd').format(expense.date);
-
-                        return Column(
-                          children: [
-                            ListTile(
-                              contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                              title: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        expense.title,
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                          color: Colors.red, // Set a color for expenses
-                                        ),
-                                      ),
-                                      SizedBox(width: 8),
-                                      Text(
-                                        '($formattedDate)',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w300,
-                                          fontSize: 14,
-                                          color: Color(0xFF474646),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '${expense.amount} \$',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                      color: Colors.red, // Set a color for expenses
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              subtitle: Text(
-                                'Expense',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            ),
-                            Divider( // Divider added here to separate ListTiles
-                              color: Colors.grey,  // Color of the divider
-                              thickness: 1,         // Thickness of the divider line
-                            ),
-                          ],
-                        );
-                      }).toList() ?? [],
-                    ),
-                    // Display incomes
-                    Column(
-                      children: graphData?.incomeList.map((income) {
-                        // Format the DateTime to string with desired format
-                        String formattedDate = DateFormat('yyyy-MM-dd').format(income.date);
-
-                        return Column(
-                          children: [
-                            ListTile(
-                              contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                              title: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        income.title,
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                          color: Colors.green, // Set a color for incomes
-                                        ),
-                                      ),
-                                      SizedBox(width: 8),
-                                      Text(
-                                        '($formattedDate)',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w300,
-                                          fontSize: 14,
-                                          color: Color(0xFF474646),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '${income.amount} \$',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                      color: Colors.green, // Set a color for incomes
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              subtitle: Text(
-                                'Income',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            ),
-                            Divider( // Divider added here to separate ListTiles
-                              color: Colors.grey,  // Color of the divider
-                              thickness: 1,         // Thickness of the divider line
-                            ),
-                          ],
-                        );
-                      }).toList() ?? [],
-                    ),
-                  ],
-                ),
-              ),
-
-            ],
+        child: _pages[_selectedIndex], // Dynamically show the selected page
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Ajouter Income',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.remove_circle_outline),
+            label: 'Ajouter Expense',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Home Page
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple.shade100,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 3),
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildInfoCard("Incomes", "150 \$", Colors.green),
+                _buildInfoCard("Expenses", "200 \$", Colors.red),
+                _buildInfoCard("Balance", "700 \$", Colors.blue),
+              ],
+            ),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            margin: const EdgeInsets.only(bottom: 15),
+            decoration: BoxDecoration(
+              color: Colors.cyan.shade50,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 3),
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            child: ListTile(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        'Salary',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        '(4 janv.)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 12,
+                          color: Color(0xFF474646),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    '1500 \$',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+              subtitle: const Text(
+                'Income',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black54,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsScreen(
+                      title: 'Salary',
+                      description: 'Monthly salary',
+                      category: 'Income',
+                      amount: 1500.0,
+                      date: DateTime(2025, 1, 4),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -248,7 +200,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Text(
             title,
             style: TextStyle(
-              fontSize: 14, // Adjusted font size
+              fontSize: 14,
               fontWeight: FontWeight.bold,
               color: color,
             ),
